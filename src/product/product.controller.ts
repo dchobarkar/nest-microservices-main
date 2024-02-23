37bca8f33337e7912c597f1539ebdf68f6e85b02
Darshan Chobarkar <dchobarkar@gmail.com>
2024-02-23 20:53:46 +0530

    add mongodb crud

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -11,8 +11,28 @@ export class ProductController {
     return this.productService.all();
   }
 
-  @EventPattern('hello')
-  async hello(data: string) {
-    console.log(data);
+  @EventPattern('product_created')
+  async productCreated(product: any) {
+    await this.productService.create({
+      id: product.id,
+      title: product.title,
+      image: product.image,
+      likes: product.likes,
+    });
+  }
+
+  @EventPattern('product_updated')
+  async productUpdated(product: any) {
+    await this.productService.update(product.id, {
+      id: product.id,
+      title: product.title,
+      image: product.image,
+      likes: product.likes,
+    });
+  }
+
+  @EventPattern('product_deleted')
+  async productDeleted(id: number) {
+    await this.productService.delete(id);
   }
 }
