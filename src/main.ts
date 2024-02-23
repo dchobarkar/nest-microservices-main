37c02408198e77ecb4901f1148b340b7529e82fc
Darshan Chobarkar <dchobarkar@gmail.com>
2024-02-23 20:31:26 +0530

    connect with rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: 'http://localhost:4200',
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amq://localhost:5672'],
+      queue: 'main_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
   });
-  await app.listen(8001);
+
+  app.listen(() => {
+    console.log('Microservice is running');
+  });
+  // const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix('api');
+  // app.enableCors({
+  //   origin: 'http://localhost:4200',
+  // });
+  // await app.listen(8001);
 }
 bootstrap();
